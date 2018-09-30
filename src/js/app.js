4180701af12c1ce109fbd837cc6666b36172131e
Carolinadelb <carolinadelgadob@gmail.com>
2018-09-30 18:57:33 -0300

    todo

---
+++ src/js/app.js
@@ -0,0 +1,53 @@
+// Sticky navbar
+$(document).ready(function () {
+  
+  var stickyToggle = function (sticky, stickyWrapper, scrollElement) {
+      var stickyHeight = sticky.outerHeight();
+      var stickyTop = stickyWrapper.offset().top;
+      if (scrollElement.scrollTop() >= stickyTop) {
+          stickyWrapper.height(stickyHeight);
+          sticky.addClass("is-sticky");
+      }
+      else {
+          sticky.removeClass("is-sticky");
+          stickyWrapper.height('auto');
+      }
+  };
+  $('[data-toggle="sticky-onscroll"]').each(function () {
+    var sticky = $(this);
+    var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+    sticky.before(stickyWrapper);
+    sticky.addClass('sticky');
+
+    // Scroll & resize events
+    $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function () {
+        stickyToggle(sticky, stickyWrapper, $(this));
+    });
+
+    // On page load
+    stickyToggle(sticky, stickyWrapper, $(window));
+});
+});
+
+//Flechita para arriba
+// ===== Scroll to Top ==== 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+//Video
